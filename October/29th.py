f06bdea5e9b467230b792aa819c0eaa999f76453
Satyam Raj <21052101@kiit.ac.in>
2023-10-06 02:55:22 +0530

    29th commit

---
+++ October/29th.py
@@ -0,0 +1,7 @@
+# Write a Python program that prints out all colors from color_list_1 that are not present in color_list_2.
+color_list_1 = set(["White", "Black", "Red"])
+color_list_2 = set(["Red", "Green"])
+
+for color in color_list_1:
+    if color not in color_list_2 :
+        print(color)
